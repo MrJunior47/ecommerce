e641293c80f5ca1d0ab4efaa67d65435441719e2
MrJunior47 <106443500+MrJunior47@users.noreply.github.com>
2023-03-01 10:41:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st # pip install streamlit
 from PIL import Image # pip install pillow
 
 # --- PATH SETTINGS ---
-THIS_DIR = Path(streamlit_app.py) if "streamlit_app.py" in locals() else Path.os.getcwd()
+THIS_DIR = Path.os.getcwd()
 ASSETS_DIR = THIS_DIR / "assets"
 STYLES_DIR = THIS_DIR / "styles"
 CSS_FILE = STYLES_DIR / "main.css"
